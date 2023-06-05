74d7e946454f9b86f37c7d35bf326aa70751703d
Diogo Pimenta <106586929+Di0g0Pimenta@users.noreply.github.com>
2023-06-05 18:29:23 +0100

    teste

---
+++ Scripts/Redirect.js
@@ -0,0 +1,7 @@
+function redirectToLoginPage() {
+    window.location.href = "http://127.0.0.1:5500/Pages/Login.html";
+}
+
+function redirectToRegisterPage() {
+    window.location.href = "http://127.0.0.1:5500/Pages/Register.html";
+}
